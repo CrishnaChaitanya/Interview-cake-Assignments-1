48d220331ca9324b4398df61bb6b748a26306d76
CrishnaChaitanya <121910302022@gitam.in>
2021-03-31 08:20:21 +0530

    3rd question

--- 03_product_of_other_numbers.py
+++ 03_product_of_other_numbers.py
@@ -1,7 +1,7 @@
-""" You have a list of integers, and for each index you want to find the product 
+""" You have a list of integers, and for each index you want to find the product
 of every integer except the integer at that index.
 
-Write a function get_products_of_all_ints_except_at_index() that takes a 
+Write a function get_products_of_all_ints_except_at_index() that takes a
 list of integers and returns a list of the products.
 
 For example, given:
@@ -19,3 +19,40 @@ by calculating:
 Here's the catch: You can't use division in your solution! """
 
 # Start coding from here
+
+
+from functools import reduce
+
+
+def get_products_of_all_ints_except_at_index(alist):
+    listOfProducts = list()
+    for i in range(len(alist)):
+        calc = 1
+        for j in range(len(alist)):
+
+            if i != j:
+                calc = calc*alist[j]
+        listOfProducts.append(calc)
+
+    return listOfProducts
+
+
+# print('by calcluating ..',
+#       get_products_of_all_ints_except_at_index([1, 7, 3, 4]))
+
+
+# The above solution is taking O(n²) TIME COMPLEXITY.
+# A much better solution would be to calc the product of the whole array at once and divide with the element at that particular index
+# soo..
+
+
+def get_products_of_all_ints_except_at_index2(alist):
+    listofproducts = [1]*len(alist)
+    store = reduce((lambda x, y: x*y), alist)
+    print(store)
+    for i in range(len(alist)):
+        listofproducts[i] = store//alist[i]
+    return listofproducts
+
+
+print(get_products_of_all_ints_except_at_index2([1, 7, 3, 4]))
